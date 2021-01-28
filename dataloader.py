5a342a4dc5e4ebadde12a4afcaeb75b660fd2fcf
Tom <LegendaryTom@users.noreply.github.com>
2021-01-28 08:09:01 -0500

    Runs validation test for ImageNet on vgg or resnet. Added custom dataloader for ImageNet dataset.

---
+++ dataloader.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 2021
+
+@author: Tom
+"""
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+class ImageNetDataset(Dataset):
+    """Custom dataset for loading ImageNet Dataset.
+    Ref: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+    Uses images in img_dir as testing dataset (X).
+    Uses labels in val.txt as target labels (Y).
+    Class ID's are in 'sysnet.txt'.
+    Class descriptions are in 'sysnet_words.txt'.
+    """
+
+    def __init__(self, img_dir, transform=None):
+        """
+        Args:
+            img_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        
+        #List of images files in img_dir
+        onlyfiles = [f for f in listdir(img_dir) if isfile(join(img_dir, f))]
+        self.image_names = onlyfiles
+        self.labels = pd.read_csv('val.txt', delimiter=' ', names=['target']).to_numpy()
+        self.img_dir = img_dir
+        self.transform = transform
+
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        image_path = join(self.img_dir, self.image_names[index])
+        img = Image.open(image_path).convert('RGB')
+        target = self.labels[index]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return {"image":img, "target":target}
+        
+        
+    
+    
---
+++ p1.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 2021
+
+@author: Tom
+
+Ref: https://calebrob.com/ml/imagenet/ilsvrc2012/2018/10/22/imagenet-benchmarking.html
+"""
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+import time
+import argparse
+from torch.utils.data import DataLoader
+from dataloader import ImageNetDataset
+
+def run_main(FLAGS):
+    # Check if cuda is available
+    use_cuda = torch.cuda.is_available()
+    
+    # Set proper device based on cuda availability 
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print("Torch device selected: ", device)
+    
+    ''' Create Model '''
+    if(FLAGS.model == 'vgg'):
+        model = models.vgg16_bn(pretrained=True, progress=True)
+    if(FLAGS.model == 'resnet'):
+        model = models.resnet34(pretrained=True, progress=True)
+    model.to(device)
+    model.eval()
+    
+    #print("Using model")
+    #print(model)
+        
+    ''' Test Network '''
+    run_test(model, FLAGS)
+
+def run_test(model, FLAGS):
+    batch_size = FLAGS.batch_size
+    transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    dataset = ImageNetDataset(FLAGS.img_dir, transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    correct = 0
+    for i, batch in enumerate(dataloader):
+        '''
+        batch['image'] is a batch of images (X)
+        batch['target'] is a batch of labels (Y)
+        '''
+        # print(i, batch.shape())
+        # print(i, batch['image'].shape)
+        # print(i, batch['target'].shape)
+        predictions = model.forward(batch['image']).argmax(dim=1, keepdim=True)
+        # print("predictions", predictions.shape, predictions)
+        # print("targets", batch['target'].shape, batch['target'])
+        correct += predictions.eq(batch['target'].view_as(predictions)).sum().item()
+        # if((i+1)%100==0):
+        print(i, "Result:", correct, "/", (i+1)*batch_size)
+    print("Result:", correct, "/", i*batch_size)
+    print("Accuracy:", correct/(i*batch_size))
+
+if __name__ == '__main__':
+    # Set parameters for Sparse Autoencoder
+    parser = argparse.ArgumentParser('CNN Exercise.')
+    parser.add_argument('--model',
+                        type=str, 
+                        default='resnet',
+                        help='(resnet or vgg) Select VGG or ResNet model.')
+    parser.add_argument('--PGD',
+                        type=str,
+                        default='off',
+                        help='(on or off) Whether to use adversarial images created with PGD or original imagenet images.')
+    parser.add_argument('--epsilon',
+                        type=int, 
+                        default=2,
+                        help='(2 to 10) Max allowed perturbation')
+    parser.add_argument('--img_dir',
+                        type=str, 
+                        default='../../Data/ImageNet2012/ILSVRC2012_img_val/',
+                        help='Directory location of ImageNet validation images')
+    parser.add_argument('--batch_size',
+                        type=int, 
+                        default=10,
+                        help='Batch size for testing network')
+
+    
+    FLAGS = None
+    FLAGS, unparsed = parser.parse_known_args()
+    print("FLAGS", FLAGS)
+
+    #Keep track of run time
+    start = time.time()
+    run_main(FLAGS)
+    end = time.time()
+    print("Elapsed Time")
+    print(end - start)
